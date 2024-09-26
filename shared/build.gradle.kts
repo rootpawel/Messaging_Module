24fdb5a94f38b51c4959769d338003d9343f03f6
Pawel Rut <pawel.rut@gmail.com>
2024-09-26 13:15:30 +0300

    jitpack

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,7 +10,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "11"
             }
         }
         publishLibraryVariants("release")
@@ -66,8 +66,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 dependencies {
